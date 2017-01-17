165dccae747c9db20ae11e258f2e704f2ce2ca6f
JerryZhang <jerry0908mo@163.com>
2017-01-17 17:56:08 +0800

    add classCertificates project

--- ClassCertificates/ClassCertificates/main.c
+++ ClassCertificates/ClassCertificates/main.c
@@ -9,7 +9,14 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    congratulateStudent("Kate", "Cocoa", 5);
+    congratulateStudent("Bo", "Objective-C", 2);
+    congratulateStudent("Mike", "Python", 5);
+    congratulateStudent("Liz", "iOS", 5);
+    return 0;
+}
+
+int congratulateStudent (char *student, char *course, int numDays) {
+    printf("%s has done as much %s Programming as I could fit into %d days.\n", student, course, numDays);
     return 0;
 }
